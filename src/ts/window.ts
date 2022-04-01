8604da10d8b0bfd98c715897b9b42fbf0d843015
Kaytotes <me@kaytotes.online>
2022-04-01 16:51:12 +0100

    Add Window Class

---
+++ src/ts/window.ts
@@ -0,0 +1,37 @@
+import { BrowserWindow } from "electron";
+
+/**
+ * Used to both Create and Access the Browser Window.
+ */
+export class Window {
+    private window: BrowserWindow;
+
+    /**
+     * Actually creates the Window for later use.
+     * @param entrypoint Typically the Webpack Main entry.
+     */
+    public create(entrypoint: string) {
+        this.window = new BrowserWindow({
+            width: 1280,
+            height: 720,
+            resizable: false,
+            maximizable: false,
+        });
+
+        this.window.loadURL(entrypoint);
+
+        this.window.setMenu(null);
+    }
+
+    /**
+     * Get the BrowserWindow instance that was created.
+     */
+    public get(): BrowserWindow {
+        return this.window;
+    }
+}
+
+/**
+ * Constant instance of the Window Class.
+ */
+export const WindowManager = new Window();
