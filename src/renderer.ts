db3f4ef9437d5a5bea117347db60a30aa0a313fd
Kaytotes <me@kaytotes.online>
2022-04-02 11:04:32 +0100

    Fix Bootstrap JS Initialising

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,12 @@
-import 'bootstrap';
+const bootstrap = require('bootstrap')
 import './scss/app.scss';
 import "@fortawesome/fontawesome-free/js/all";
+
+/** Enable Tooltips */
+let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+tooltipTriggerList.map(function (tooltipTriggerEl: any) {
+    console.log(tooltipTriggerEl);
+
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
