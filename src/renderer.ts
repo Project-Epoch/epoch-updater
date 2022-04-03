37cd396cb71ef7e31ac087cf11b9ae6d6f3b1ce9
Kaytotes <me@kaytotes.online>
2022-04-03 23:46:38 +0100

    Remove a Console Log

--- src/renderer.ts
+++ src/renderer.ts
@@ -5,8 +5,6 @@ import "@fortawesome/fontawesome-free/js/all";
 /** Enable Tooltips */
 let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 tooltipTriggerList.map(function (tooltipTriggerEl: any) {
-    console.log(tooltipTriggerEl);
-
     return new bootstrap.Tooltip(tooltipTriggerEl);
 });
 
