373071c6132a7f4a842cdd858050e6086e683022
Kaytotes <me@kaytotes.online>
2022-04-09 19:33:11 +0100

    Add Updater

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { app } from 'electron';
+import { app, autoUpdater } from 'electron';
 import Main from './ts/backend/main';
 import unhandled from 'electron-unhandled';
 
@@ -8,6 +8,13 @@ import unhandled from 'electron-unhandled';
 if (require('electron-squirrel-startup')) {
     // eslint-disable-line global-require
     app.quit();
+}
+
+const server = "electron-deploy-j1bpx82a5-kaytotes.vercel.app"
+const url = `${server}/update/${process.platform}/${app.getVersion()}`;
+
+if (app.isPackaged) {
+    autoUpdater.setFeedURL({ url });
 }
 
 unhandled();
